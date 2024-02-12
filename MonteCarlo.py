5024bca7ef71af0bae6da31a3ad11ca02ad37c6e
erizheng <114521575+erizheng@users.noreply.github.com>
2024-02-11 17:49:12 -0800

    Update MonteCarlo.py

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import *
 import random
+import time
 from sympy import var, Sum, pi, limit, oo
 import matplotlib.pyplot as plt
 import scienceplots
@@ -12,6 +13,8 @@ plt.style.use(['science', 'notebook'])
 #c, is variance, sig^2
 def prob_norm_dist(a, b, c):
     return 1/(2* np.pi *c) * np.exp( - (a-b)**2 / (2 * c))
+
+
 
 #
 def samp_norm_dist(b):
@@ -102,7 +105,6 @@ def obs_model(z_t, x_t, landmarks):
 
 
 
-
 #X_prev, set of particle
 #m, numbers of particles in set X_prev
 #z_t, relative location given by sensors
@@ -134,9 +136,69 @@ def MCL(Xprev, u_t, z_t, m):
 
     return Xt
 
-
-
 #running
 p = np.random.randint(0, 100, size=1000)
 z = [3, 2, 17] #temp values, don't know what to do with z actually
 MCL(p, 0, z, len(p))
+
+
+############2D implementation
+
+class Position_vector:
+    def __init__(self, x, y, angle, time):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.time = time
+
+class Velocity_vector:
+    def __init__(self, v, w, time):
+        self.v = v
+        self.w = w
+        self.time = time
+
+
+#similar to prob_norm_dist
+def prob_norm(a,b):
+    return 1/(np.sqrt(2*np.pi *b)) * np.exp(-(a**2)/2*b)
+
+#weigthed values for robot speci motion error
+alpha1 = 1
+alpha2 = 1
+alpha3 = 1
+alpha4 = 1
+alpha5 = 1
+alpha6 = 1
+
+#based off page 97
+def prob_vel_model(x_t, u_t, x_prev):
+    mew = (1/2)(((x_prev.x - x_t.x)*np.cos(x_prev.angle) + (x_prev.y - x_t.y)*np.sin(x_prev.angle))/((x_prev.y - x_t.y)*np.cos(x_prev.angle) - (x_prev.x - x_t.x)*np.sin(x_prev.angle)))
+
+    x_star = ((x_prev.x + x_t.x))/(2) + mew*(x_prev.y + x_t.y)
+    y_star = ((x_prev.y + x_t.y))/(2) + mew*(x_prev.x + x_t.x)
+
+    r_star = np.sqrt((x_prev.x - x_star)**2 + (x_prev.y - y_star)**2)
+
+    change_delta = np.arctan2(x_t.y - y_star, x_t.x - x_star) - np.arctan2(x_prev.y - y_star, x_prev.x - x_star)
+    change_time = x_t.time - x_prev.time
+
+    v_hat = (change_delta/change_time)* r_star
+    w_hat = (change_delta/change_time)
+    gamma = (x_t.angle - x_prev.angle)/change_time - w_hat
+
+    return prob_norm(u_t.v - v_hat, alpha1*np.abs(u_t.v) + alpha2*np.abs(u_t.w)) * prob_norm(u_t.w - w_hat, alpha3*np.abs(u_t.v) + alpha4*np.abs(u_t.w)) * prob_norm(gamma, alpha5*np.abs(u_t.v) + alpha6*np.abs(u_t.w))
+
+def motion_velocity_control(u_t, x_prev):
+    v_hat = u_t.v + samp_norm_dist(alpha1*np.abs(u_t.v) + alpha2*np.abs(u_t.w))
+    w_hat = u_t.w + samp_norm_dist(alpha3*np.abs(u_t.v) + alpha4*np.abs(u_t.w))
+    gamma = samp_norm_dist(alpha5*np.abs(u_t.v) + alpha6*np.abs(u_t.w))
+
+    time = time.time()
+
+    x_next = x_prev.x - (v_hat/w_hat)*np.sin(x_prev.angle) + (v_hat/w_hat)*np.sin(x_prev.angle + w_hat*(time - x_prev.time))
+    y_next = x_prev.y + (v_hat/w_hat)*np.cos(x_prev.angle) - (v_hat/w_hat)*np.cos(x_prev.angle + w_hat*(time - x_prev.time))
+
+    angle_next = x_prev.angle + w_hat*(time - x_prev.time) + gamma*(time - x_prev.time)
+
+    return Position_vector(x_next, y_next, angle_next, time)
+
